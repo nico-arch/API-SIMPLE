4c9897e31f6a9fe49ca5fad35384f240625d3f42
Nick Anderson Azémar <76443393+nico-arch@users.noreply.github.com>
2024-04-24 18:07:58 +0000

    Setting up for production

--- src/server.js
+++ src/server.js
@@ -1,12 +1,33 @@
 import express from 'express';
-import mainRoutes from './main.routes.js'
-import userRoutes from './user.rootes.js'
+import helmet from 'helmet';
+import { rateLimit } from 'express-rate-limit';
 import { StatusCodes } from 'http-status-codes';
+import compression from 'compression';
+
+
+import mainRoutes from './main.routes.js';
+import userRoutes from './user.rootes.js';
+
+
+const limiter = rateLimit({
+  //windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 60 * 1000, // 1 minute
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 1 minute).
+  validate: { xForwardedForHeader: false }
+  //message: 'Too many requests from this IP, please try again after 1 minute',
+})
 
 const app = express();
 const port = 3000;
 
+app.use(compression());
+// Apply the rate limiting middleware to all requests.
+app.use(limiter);
 app.use(express.json());
+app.use(helmet());
+
+
+
 
 app.use('/v1', mainRoutes)
 app.use('/v1/user', userRoutes)
